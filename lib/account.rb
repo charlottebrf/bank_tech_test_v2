ff1fe0075fed4139298c4c28b4e204744eb2579f
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-11 14:04:08 +0100

    Adds a #money_converter to Account to ensure #add_deposit is accurate to two decimal places

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,16 @@
 # understands the interpretation of Client commands
 class Account
 
+def money_converter(quantity)
+  converted = "%.2f" % (quantity / 1.00)
+  converted.to_i
+end
+
 def add_deposit(balance, credit)
+  balance = money_converter(balance)
+  credit = money_converter(credit)
   balance += credit
 end
+
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,9 +5,19 @@ require 'account'
 describe Account do
   subject(:account) { described_class.new }
 
+  describe '#money_converter' do
+    it 'converts integers to be accurate to currency' do
+      expect(account.money_converter(10)).to eq 10.00
+    end
+  end
+
   describe '#add_deposit' do
-    it "adds a client\'s credit to the account balance" do
-      expect(account.add_deposit(5, 10)).to eq 15
+    it 'adds a client\'s credit to the account balance' do
+      expect(account.add_deposit(5, 10)).to eq 15.00
+    end
+
+    it 'always has a return accurate to two decimal places' do
+      expect(account.add_deposit(5.00, 10.00)).to eq 15.00
     end
   end
   #
